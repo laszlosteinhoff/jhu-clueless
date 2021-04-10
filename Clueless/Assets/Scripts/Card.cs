a3b514d9267d47fdb931e5f17674827d25f7a6c8
Jake Strang <jake@jstrang.com>
2021-04-09 23:25:26 -0400

    Added weapons, characters, and room tiles. Hooked up UI manager to start game request. Populated game deck

---
+++ Clueless/Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    // These have all been passed in using the Unity UI, they don't need to be constructed
+    public int id;
+    public string cardName;
+    public bool disproven;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Clueless/Assets/Scripts/Game.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // These have all been passed in using the Unity UI, they don't need to be constructed
+    public List<Card> deck;
+}
